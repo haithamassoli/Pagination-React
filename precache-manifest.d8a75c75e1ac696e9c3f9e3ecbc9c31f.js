79e17ab5f46f1e93fc3847e885302150b8e200fc
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-16 15:29:47 +0200

    Updates

---
+++ precache-manifest.d8a75c75e1ac696e9c3f9e3ecbc9c31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a3ef0d0256fd21082e8cace1e86619",
+    "url": "/Pagination-React/index.html"
+  },
+  {
+    "revision": "67cb30cc66d2d2887960",
+    "url": "/Pagination-React/static/css/main.980c564c.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Pagination-React/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pagination-React/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cb30cc66d2d2887960",
+    "url": "/Pagination-React/static/js/main.de5ae76c.chunk.js"
+  },
+  {
+    "revision": "095da77f03dadf433760",
+    "url": "/Pagination-React/static/js/runtime-main.2e8d789c.js"
+  }
+]);
